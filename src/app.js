49d34bdc98a4ec30ba62171ff9d61d3c15aff428
Romain-14 <romain14f@msn.com>
2024-12-23 10:47:36 +0100

    update to deploy on render

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import router from "./router/index.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
-const DOMAIN = process.env.DOMAIN;
+// const DOMAIN = process.env.DOMAIN;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "src/views"));
@@ -17,5 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 app.listen(PORT, () => {
-	console.log(`Running at http://${DOMAIN}:${PORT}`);
+	// console.log(`Running at http://${DOMAIN}:${PORT}`);
+    console.log("Running")
 });
